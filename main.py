8fd6be95eb5e9c2fc7dc9afaf28418e12259ff58
kkazu2212 <kkwx2015@gmail.com>
2022-12-05 22:35:48 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import time
+
+st.title('Streamlit 超入門')
+
+st.write('プログレスバーの表示')
+'Start!'
+
+latest_interation = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_interation.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.05)
+
+'Done!!!!!'
+
+option = st.selectbox(
+    '好きな数字は？',
+    list(range(1, 11))
+)
+'好きな数字は、',option, 'です。'
+
+st.write('Interactive Widgets')
+
+text = st.sidebar.text_input('あなたの趣味をおしえてください。')
+condition = st.sidebar.slider('調子は？', 0, 100, 50)
+
+'あなたの趣味：',text
+'調子：', condition
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
